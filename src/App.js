03121e7c662079875e0e3a21f418b865d65b8cfa
JC Grennan <jgrennan94@gmail.com>
2022-09-28 16:57:02 +0100

    initialised state for time remaining

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, {useState} from "react"
 function App() {
 
   const [text, setText] = useState("")
+  const [timeRemaining] = useState(30)
 
   function handleChange(event) {
     setText(event.target.value)
@@ -17,7 +18,7 @@ function App() {
     <div>
       <h1>Speed Typing Game</h1>
       <textarea value={text} onChange={handleChange}/>
-      <h4>Time Remaining :</h4>
+      <h4>Time Remaining : {timeRemaining}</h4>
       <button onClick={() => console.log(countWords(text))}>Start</button>
       <h1>Word Count: </h1>
     </div>
