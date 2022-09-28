ca3995c02cab166967930a0fc882dba45e8ba966
JC Grennan <jgrennan94@gmail.com>
2022-09-28 16:29:51 +0100

    set up state and func for the textarea

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import React, {useState} from "react"
+
 function App() {
+
+  const [text, setText] = useState("")
+
+  function handleChange(event) {
+    setText(event.target.value)
+  }
+  
+  console.log(text)
+  
   return (
     <div>
       <h1>Speed Typing Game</h1>
-      <textarea />
+      <textarea value={text} onChange={handleChange}/>
       <h4>Time Remaining :</h4>
       <button>Start</button>
       <h1>Word Count: </h1>
