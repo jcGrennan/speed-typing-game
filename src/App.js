7739c8f72b1128c6236120ac098808a4ee67b27f
JC Grennan <jgrennan94@gmail.com>
2022-09-28 17:32:23 +0100

    set the timer to count down when start is clicked

--- src/App.js
+++ src/App.js
@@ -4,32 +4,35 @@ function App() {
 
   const [text, setText] = useState("")
   const [timeRemaining, setTimeRemaining] = useState(5)
+  const [isTimeRunning, setIsTimeRunning] = useState(false)
 
   function handleChange(event) {
     setText(event.target.value)
   }
 
-  function countWords(string) {
-    const wordsArr = string.trim().split(" ")
-    return wordsArr.filter(word => word !== "").length
-  }
+  // function countWords(string) {
+  //   const wordsArr = string.trim().split(" ")
+  //   return wordsArr.filter(word => word !== "").length
+  // }
 
   useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      setTimeRemaining(prevTime => prevTime - 1)
-    }, 1000)
-
     if(timeRemaining === 0) {
-      clearTimeout(timeoutId)
+      setIsTimeRunning(false)
     }
-  }, [timeRemaining])
+    if(isTimeRunning && timeRemaining > 0) {
+      setTimeout(() => {
+      setTimeRemaining(prevTime => prevTime - 1)
+      }, 1000)
+    }
+
+  }, [isTimeRunning, timeRemaining])
 
   return (
     <div>
       <h1>Speed Typing Game</h1>
       <textarea value={text} onChange={handleChange}/>
       <h4>Time Remaining : {timeRemaining}</h4>
-      <button onClick={() => console.log(countWords(text))}>Start</button>
+      <button onClick={() => setIsTimeRunning(true)}>Start</button>
       <h1>Word Count: </h1>
     </div>
   );
