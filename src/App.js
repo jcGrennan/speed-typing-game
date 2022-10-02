4b9fdeed2e334f1e9c1eed98a5343ed2befe3291
JC Grennan <jgrennan94@gmail.com>
2022-10-02 17:54:28 +0100

    imported and used custom hook; useLogic

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-import React, {useState, useEffect, useRef, useCallback} from "react"
+import useLogic from "./useLogic";
 
 function App() {
 
-  return (
-    <div>
-      <h1>Can you type a word per second???</h1>
-      <textarea ref={textAreaRef} disabled={!isTimeRunning} value={text} onChange={handleChange}/>
-      <h4>Time Remaining : {timeRemaining}</h4>
-      <button disabled={isTimeRunning} onClick={startGame}>Start</button>
-      <h1>Word Count: {wordCount}</h1>
-    </div>
-  );
+    const [
+        text, 
+        timeRemaining, 
+        isTimeRunning, 
+        wordCount, 
+        textAreaRef, 
+        handleChange, 
+        startGame
+    ] = useLogic()
+
+    return (
+        <div>
+            <h1>Can you type a word per second???</h1>
+
+            <textarea 
+                ref={textAreaRef} 
+                disabled={!isTimeRunning} 
+                value={text} 
+                onChange={handleChange}
+            />
+
+            <h4>Time Remaining : {timeRemaining}</h4>
+
+            <button 
+                disabled={isTimeRunning} 
+                onClick={startGame}
+            >
+                Start
+            </button>
+
+            <h1>Word Count: {wordCount}</h1>
+        </div>
+    );
 }
 
 export default App;
